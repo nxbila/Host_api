630d21272e84054c86cb4cc3e7a9c52ba3b451f8
nxbila <nabila.akhtar99@gmail.com>
2024-12-24 18:10:57 +0300

    checking uri

--- database/connect.js
+++ database/connect.js
@@ -1,7 +1,15 @@
+require("dotenv").config()
 const mongoose = require("mongoose")
 // const uri = "mongodb+srv://nabilaakhtar99:nab%40DB12@cluster0.fx1zl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const connectDB = (uri) => {
+  console.log('MONGODB_URI:', process.env.MONGODB_URI); // Add this for debugging
+
+
+  if (!uri) {
+  console.error('MongoDB URI is not defined');
+  process.exit(1); // Exit the process if the URI is not available
+  }
     console.log("Connected to DB");
     
     return mongoose.connect(uri).then(res=>{
